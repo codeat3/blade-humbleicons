93fa6dd355e3e0c2fe691db918d8b8410a0f0a8a
Swapnil Sarwe <swapnilsarwe@gmail.com>
2024-06-02 01:52:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,92 @@
+<p align="center">
+    <img src="./socialcard-blade-humbleicons.png" width="1280" title="Social Card Blade Humbleicons">
+</p>
+
+# Blade Humbleicons
+
+<a href="https://github.com/codeat3/blade-humbleicons/actions?query=workflow%3ATests">
+    <img src="https://github.com/codeat3/blade-humbleicons/workflows/Tests/badge.svg" alt="Tests">
+</a>
+<a href="https://packagist.org/packages/codeat3/blade-humbleicons">
+    <img src="https://img.shields.io/packagist/v/codeat3/blade-humbleicons" alt="Latest Stable Version">
+</a>
+<a href="https://packagist.org/packages/codeat3/blade-humbleicons">
+    <img src="https://img.shields.io/packagist/dt/codeat3/blade-humbleicons" alt="Total Downloads">
+</a>
+
+A package to easily make use of [Humbleicons](https://github.com/zraly/humbleicons) in your Laravel Blade views.
+
+For a full list of available icons see [the SVG directory](resources/svg) or preview them at [humbleicons.com](https://humbleicons.com/).
+
+## Requirements
+
+- PHP 7.4 or higher
+- Laravel 8.0 or higher
+
+## Installation
+
+```bash
+composer require codeat3/blade-humbleicons
+```
+
+## Updating
+
+Please refer to [`the upgrade guide`](UPGRADE.md) when updating the library.
+
+## Blade Icons
+
+Blade Humbleicons uses Blade Icons under the hood. Please refer to [the Blade Icons readme](https://github.com/blade-ui-kit/blade-icons) for additional functionality. We also recommend to [enable icon caching](https://github.com/blade-ui-kit/blade-icons#caching) with this library.
+
+## Configuration
+
+Blade Humbleicons also offers the ability to use features from Blade Icons like default classes, default attributes, etc. If you'd like to configure these, publish the `blade-humbleicons.php` config file:
+
+```bash
+php artisan vendor:publish --tag=blade-humbleicons-config
+```
+
+## Usage
+
+Icons can be used as self-closing Blade components which will be compiled to SVG icons:
+
+```blade
+<x-humble-aid/>
+```
+
+You can also pass classes to your icon components:
+
+```blade
+<x-humble-aid class="w-6 h-6 text-gray-500"/>
+```
+
+And even use inline styles:
+
+```blade
+<x-humble-aid style="color: #555"/>
+```
+
+### Raw SVG Icons
+
+If you want to use the raw SVG icons as assets, you can publish them using:
+
+```bash
+php artisan vendor:publish --tag=blade-humbleicons --force
+```
+
+Then use them in your views like:
+
+```blade
+<img src="{{ asset('vendor/blade-humbleicons/aid.svg') }}" width="10" height="10"/>
+```
+
+## Changelog
+
+Check out the [CHANGELOG](CHANGELOG.md) in this repository for all the recent changes.
+
+## Maintainers
+
+Blade Humbleicons is developed and maintained by [Swapnil Sarwe](https://swapnilsarwe.com).
+
+## License
+
+Blade Humbleicons is open-sourced software licensed under [the MIT license](LICENSE.md).
---
+++ tests/CompilesIconsTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Facades\Config;
+use BladeUI\Icons\BladeIconsServiceProvider;
+use Codeat3\BladeHumbleicons\BladeHumbleiconsServiceProvider;
+
+class CompilesIconsTest extends TestCase
+{
+    /** @test */
+    public function it_compiles_a_single_anonymous_component()
+    {
+        $result = svg('humble-aid')->toHtml();
+
+        // Note: the empty class here seems to be a Blade components bug.
+        $expected = <<<'SVG'
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 8H6a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8a2 2 0 0 0-2-2h-2M8 8V6a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M8 8h8m-4 4v2m0 0v2m0-2h-2m2 0h2"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_classes_to_icons()
+    {
+        $result = svg('humble-aid', 'w-6 h-6 text-gray-500')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 8H6a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8a2 2 0 0 0-2-2h-2M8 8V6a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M8 8h8m-4 4v2m0 0v2m0-2h-2m2 0h2"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_styles_to_icons()
+    {
+        $result = svg('humble-aid', ['style' => 'color: #555'])->toHtml();
+
+        $expected = <<<'SVG'
+            <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 8H6a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8a2 2 0 0 0-2-2h-2M8 8V6a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M8 8h8m-4 4v2m0 0v2m0-2h-2m2 0h2"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_default_class_from_config()
+    {
+        Config::set('blade-humbleicons.class', 'awesome');
+
+        $result = svg('humble-aid')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="awesome" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 8H6a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8a2 2 0 0 0-2-2h-2M8 8V6a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M8 8h8m-4 4v2m0 0v2m0-2h-2m2 0h2"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_merge_default_class_from_config()
+    {
+        Config::set('blade-humbleicons.class', 'awesome');
+
+        $result = svg('humble-aid', 'w-6 h-6')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="awesome w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 8H6a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8a2 2 0 0 0-2-2h-2M8 8V6a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M8 8h8m-4 4v2m0 0v2m0-2h-2m2 0h2"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            BladeIconsServiceProvider::class,
+            BladeHumbleiconsServiceProvider::class,
+        ];
+    }
+}
